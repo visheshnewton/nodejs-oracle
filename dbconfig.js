41579015411a882ff36ac71668901b1bfdea1778
visheshnewton <165644956+visheshnewton@users.noreply.github.com>
2024-05-26 07:21:25 +0530

    proper connection and row fetched

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     user: 'ipfm',
     password: 'password',
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || '192.168.1.78/ipfm',
+    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || '192.168.1.78/orcl',
     externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
 
 };
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const dbcon = require('./dbconfig.js');
 const oracledb = require('oracledb');
 
-oracledb.initOracleClient({libDir: 'C:\\work\\instantclient\\instantclient_21_13'});
+console.log('Getting path set for oracle');
+oracledb.initOracleClient({libDir: 'C:\\WORK\\instantclient\\instantclient_21_13'});
+
+Connection = null;
 
 async function dbconnect(){
-    Connection = await oracledb.getConnection(dbcon);
-    Connection.execute('select sysdate from dual');
+    connection = await oracledb.getConnection(dbcon);
+    response = await connection.execute('SELECT FUNC_NAME from IP_FUNCTION WHERE FUNC_ID = \'F039\'');
+    console.log(response.rows);
     // Connection.execute('delete/insert');
     // Connection.execute('commit')
 
 }
 
+async function disconnect(){
+    if(Connection){
+        Connection.close();
+    }
+}
+
+try{
+    dbconnect();
+}catch(err){
+    console.log(err);
+}finally{
+    disconnect();
+}
